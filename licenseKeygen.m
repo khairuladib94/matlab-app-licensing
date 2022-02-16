ac000ee09355fd891d4efebf0927b2e252655b9b
Adib Yusof <adib.yusof@upm.edu.my>
2022-02-17 02:14:59 +0800

    Add ability to use dashes between key chars

--- licenseKeygen.m
+++ licenseKeygen.m
@@ -1,12 +1,16 @@
 function GeneratedKey = licenseKeygen(CharNum, Optn)
 % Generates a random key of numbers or letters or both.
 arguments
-    CharNum (1,:) double = 16
+    CharNum (1,:) {mustBeInteger, mustBePositive} = 16
     Optn.UseNumber (1,1) logical = true
     Optn.UseLetter (1,1) logical = true
+    Optn.PartLength (1,1) {mustBeInteger, mustBeNonnegative} = 4;
 end
 Mixture = Optn.UseNumber + Optn.UseLetter;
-assert(Mixture > 0, "UseNumber and UseLetter cannot both be false.")
+assert(Mixture > 0, "UseNumber and UseLetter cannot both be false.");
+if Optn.PartLength > 0
+    assert(rem(CharNum, Optn.PartLength) == 0, "CharNum must be a multiple of PartLength, or set PartLength to 0.")
+end
 Letters = []; 
 Numbers = [];
 if Optn.UseLetter
@@ -18,4 +22,10 @@ end
 RandIdx = randperm(CharNum);
 Key = [Letters, Numbers];
 GeneratedKey = Key(RandIdx);
-
+if Optn.PartLength == 0
+    return;
+end
+GeneratedKey = reshape(GeneratedKey, Optn.PartLength, []);
+GeneratedKey = [GeneratedKey; repmat('-', [1, width(GeneratedKey)])];
+GeneratedKey = reshape(GeneratedKey, 1, []);
+GeneratedKey(end) = [];
