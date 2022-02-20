38b78bcda91f85d157d322829c1e973c5232c966
Adib Yusof <adib.yusof@upm.edu.my>
2022-02-21 00:49:54 +0800

    Initial commit `githubGet` function

---
+++ githubGet.m
@@ -0,0 +1,33 @@
+function FileDir = githubGet(UserName, RepoName, FilePath, Optn)
+% Downloads a file from a GitHub repo, even a private one. 
+% Uses MATLAB built-in functions called webread and websave that rely on RESTful API.
+% 
+% Required arguments
+%     UserName (1,:) char : GitHub's username
+%     RepoName (1,:) char : The repo name
+%     FilePath (1,:) char : The file name or its relative path if it resides in a folder
+% 
+% Optional arguments    
+%     Token (1,:) char    : Personal Access Token (PAT), generate from https://github.com/settings/tokens. Required for a private repo. 
+% 
+% Written by Adib Yusof (2022) | adib.yusof@upm.edu.my
+arguments
+    UserName (1,:) char
+    RepoName (1,:) char
+    FilePath (1,:) char
+    Optn.Token (1,:) char = ''
+end
+BaseAPI = 'https://api.github.com';
+URL = [BaseAPI, '/repos/', UserName, '/', RepoName, '/contents/', FilePath];
+HeaderFields = {};
+if ~isempty(Optn.Token)
+    HeaderFields = {'Authorization', ['token ', Optn.Token]};
+end
+HeaderFields = [HeaderFields; {'Accept', 'application/vnd.github.v3+json'}];
+Options = weboptions(HeaderFields = HeaderFields, ContentType = 'json' );
+Data = webread(URL, Options);
+HeaderFields = {'Accept', 'application/vnd.github.v3+json'};
+Options = weboptions('HeaderFields', HeaderFields);
+FileDir = websave(FilePath, Data.download_url, Options);
+end
+
