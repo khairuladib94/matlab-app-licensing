e14be502a033a40ada0fdf428de0a9bcb1eccafd
Adib Yusof <adib.yusof@upm.edu.my>
2022-03-06 14:11:28 +0800

    Auto-uploading license database to repo

--- createLicenseDatabase.m
+++ createLicenseDatabase.m
@@ -1,8 +1,16 @@
-function Response = createLicenseDatabase(inputArg2)
+function Response = createLicenseDatabase(UserName, RepoName, Token)
 % Create a *.csv file to store all license keys and associated details
 DatabaseHeader = {'LicenseKey', 'UserEmail', 'ValidUntil' ,'DeviceID'};
-writecell(DatabaseHeader, 'license-database.csv');
-Response = githubPost(UserName, RepoName, FileRelativePath, LocalFilePath, Token, CommitMessage = 'Create license database file.');
-
+FileName = 'license-database.csv';
+writecell(DatabaseHeader, FileName);
+try
+    Response = githubPost(UserName, RepoName, FileName, FileName, ...
+        Token, CommitMessage = 'Create license database file');
+    delete(FileName);
+    disp('File successfully created and uploaded to the repo at %s', Response.commit.url);
+catch ME
+    delete(FileName);
+    error('Failed uploading the file. File with the same name possibly already exists in the repo.\n\n%s',  ME.message);
+end
 end
 
