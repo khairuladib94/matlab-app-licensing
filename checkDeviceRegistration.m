f0b5ea8999d99f97b6e52f69bc2375083450b257
Adib Yusof <adib.yusof@upm.edu.my>
2022-03-09 18:34:57 +0800

    Move block to `findUIFigure` function

--- checkDeviceRegistration.m
+++ checkDeviceRegistration.m
@@ -5,29 +5,20 @@ arguments
     RepoName
     Token
 end
+centerAppWindow(MainApp);
 IsCloseSelected = false;
 DeviceID = generateDeviceId;
-
+MainAppUIFigure = findUIFigure(MainApp);
+AppName = MainAppUIFigure.Name;
 try Lic = delimited2table(githubGet(UserName, RepoName, 'license-database.csv', Token = Token));
     MatchedDeviceIdx = find(Lic.DeviceID == DeviceID);
     if ~isempty(MatchedDeviceIdx) && any(datetime(Lic.ValidUntil(MatchedDeviceIdx)) > datetime)
         return;     % License is registered and still valid
     end
 
-    MainAppProperties = properties(MainApp);
-    IsUIFigure = false;
-    for p = 1 : numel(MainAppProperties)
-        Property = MainApp.(MainAppProperties{p});
-        IsUIFigure = isa(Property, 'matlab.ui.Figure');
-        if IsUIFigure
-            MainAppUIFigure = Property;
-            break;
-        end
-    end
-    
-    AppName = MainAppUIFigure.Name;
     if isempty(MatchedDeviceIdx)    % License is not registered
-        Selection = uiconfirm(MainAppUIFigure, 'App is not activated.', AppName, Options = {'Activate', 'Close'});
+        Selection = uiconfirm(MainAppUIFigure, 'App is not activated.', AppName, ...
+            Options = {'Activate', 'Close'});
     elseif ~isempty(MatchedDeviceIdx) && ~any(datetime(Lic.ValidUntil(MatchedDeviceIdx)) > datetime)     % License is registered but not valid
             Selection = uiconfirm(MainAppUIFigure, 'The license has expired. Please activate a new license.', AppName, Options = {'Activate', 'Close'});
     end
