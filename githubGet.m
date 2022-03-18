d4c0df8a247128c2cf03d65ecca329751b4eac9a
Adib Yusof <adib.yusof@upm.edu.my>
2022-03-18 20:31:19 +0800

    Add description for OutputType = 'status' argument

--- githubGet.m
+++ githubGet.m
@@ -6,7 +6,8 @@ function [Output, GetRequest] = githubGet(UserName, RepoName, FilePath, OutputTy
 %     UserName (1,:) char   : GitHub's username
 %     RepoName (1,:) char   : The repo name
 %     FilePath (1,:) char   : The file name or its relative path if it resides in a folder
-%     OutputType (1,:) char : 'read' (default) or 'save'; 'read' returns contents of the file, 'save' downloads the file.
+%     OutputType (1,:) char : 'read' (default), 'save' or status; 'read' returns contents of the file, 'save' downloads the file.
+%                              'status' returns whether the repo (FilePath = '') or a file in the repo (FilePath = <FileName>) exists (logical)
 % 
 % Optional arguments (name-value pair)   
 %     Token (1,:) char      : Personal Access Token (PAT), generate from https://github.com/settings/tokens. Required for a private repo. 
@@ -23,7 +24,7 @@ arguments
 end
 BaseAPI = 'https://api.github.com';
 URL = [BaseAPI, '/repos/', UserName, '/', RepoName, '/contents/', FilePath];
-if strcmp(OutputType, 'status')
+if strcmp(OutputType, 'status') && isempty(char(FilePath))
     URL = [BaseAPI, '/repos/', UserName, '/', RepoName];
 else
     if ~isempty(Optn.Branch)
